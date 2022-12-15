4f051057df0cf842bbe006d9b4d3fa75214f1af2
alessandrobelottidev <alessandro.belotti2004@gmail.com>
2022-12-15 17:11:54 +0100

    ...

--- src/server.js
+++ src/server.js
@@ -7,8 +7,7 @@ const cors = require('cors')
 const routes = require('@routes/index')
 
 const app = express()
-// const interface = process.env.LISTEN_INTERFACE
-const port = process.env.PORT || process.env.LISTEN_PORT || 3000
+const port = process.env.PORT || 3000
 
 const corsOptions = { origin: 'http://localhost:5173', credentials: true }
 const sessionOptions = {
