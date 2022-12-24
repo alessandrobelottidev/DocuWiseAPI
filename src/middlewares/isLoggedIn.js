5793d6263b5295347e7105fa6da69e2ee4734a95
Alessandro Belotti <48164098+alessandrobelottidev@users.noreply.github.com>
2022-12-24 15:44:01 +0100

    test

--- src/middlewares/isLoggedIn.js
+++ src/middlewares/isLoggedIn.js
@@ -1,4 +1,5 @@
 const isLoggedIn = (req, res, next) => {
+	console.log(req.session)
 	if (req.session.loggedIn) next()
 	else return res.status(401).json({ message: 'You need to be authenticated' })
 }
