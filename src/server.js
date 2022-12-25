497bc002f5e8bb2b6b616a36ba4d6db7c34e4546
Alessandro Belotti <48164098+alessandrobelottidev@users.noreply.github.com>
2022-12-25 02:26:29 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const corsOptions = {
 		if (allowedOriginsList.indexOf(origin) !== -1) {
 			callback(null, true)
 		} else {
-			callback(new Error('Not allowed by CORS'))
+			callback(new Error(origin + ' -> Not allowed by CORS'))
 		}
 	},
 	credentials: true,
